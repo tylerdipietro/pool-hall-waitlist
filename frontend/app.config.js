16e88077d8aaf6b19c25075e0eba362a8bb4be35
Dippy <tdipietro87@gmail.com>
2025-06-03 20:21:03 -0400

    Fixed app.config.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -6,10 +6,10 @@ export default {
     sdkVersion: "50.0.0",
     platforms: ["ios", "android", "web"],
     extra: {
-       apiBaseUrl: "https://pool-hall-waitlist-3b8c64cbf25d.herokuapp.com",
-      eas: {
-        projectId: "aa45cd69-a930-432f-a3ab-77a197e338b6"
-      }
+      apiBaseUrl: "https://pool-hall-waitlist-3b8c64cbf25d.herokuapp.com"
+    },
+    eas: {
+      projectId: "aa45cd69-a930-432f-a3ab-77a197e338b6"
     },
     updates: {
       url: "https://u.expo.dev/aa45cd69-a930-432f-a3ab-77a197e338b6"
