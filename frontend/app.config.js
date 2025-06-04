1364509993650e41ab157c332e990d114ca6588d
Dippy <tdipietro87@gmail.com>
2025-06-03 20:03:44 -0400

    Change from localhost to web base

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -6,6 +6,7 @@ export default {
     sdkVersion: "50.0.0",
     platforms: ["ios", "android", "web"],
     extra: {
+       apiBaseUrl: "https://pool-hall-waitlist-3b8c64cbf25d.herokuapp.com",
       eas: {
         projectId: "aa45cd69-a930-432f-a3ab-77a197e338b6"
       }
